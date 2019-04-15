1e21082c66240f4623f231ce4e351f4b51f52e5b
Julian Moreno <julian.moreno@globallogic.com>
2019-04-15 14:43:14 -0300

    master Se sube prueba de bootstrap

---
+++ Proyecto_Prac.Super/universidad/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component, Button } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <button className="btn btn-primary">Primario</button>
+        <p>pepe</p>
+      </div>
+          
+    );
+  }
+}
+
+export default App;
