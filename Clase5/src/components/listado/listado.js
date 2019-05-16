c0e61f6ff4865e1cf2ddef2e2e8cfec787f81581
alumno <alumno>
2019-05-15 21:13:42 -0300

    Se sube ultima actualizacion

--- Clase5/src/components/listado/listado.js
+++ Clase5/src/components/listado/listado.js
@@ -2,30 +2,24 @@ import React, { Component } from 'react';
 
 class Listado extends Component {
     render() {
+        let { data } = this.props
         return (
             <div>
                 <h1>Listado</h1>
-                <table class="egt">
-                    <tr>
-                        <th>Hoy</th>
-                        <th>Mañana</th>
-                        <th>Viernes</th>
-                    </tr>
-                    <tr>
-                        <td>Soleado</td>
-                        <td>Mayormente soleado</td>
-                        <td>Parcialmente nublado</td>
-                    </tr>
-                    <tr>
-                        <td>19°C</td>
-                        <td>17°C</td>
-                        <td>12°C</td>
-                    </tr>
-                    <tr>
-                        <td>E 13 km/h</td>
-                        <td>E 11 km/h</td>
-                        <td>S 16 km/h</td>
-                    </tr>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Area</th>
+                            <th>Name</th>
+                            <th>Capital</th>
+                        </tr>
+                        {data && data.map((row,index ) => (
+                            <tr key={index}>
+                                <td>{row.area}</td>
+                                <td>{row.name}</td>
+                                <td>{row.capital}</td>
+                            </tr>))}
+                    </tbody>
                 </table>
             </div>
         );
--- Clase5/src/components/listado/listadoC.js
+++ Clase5/src/components/listado/listadoC.js
@@ -2,9 +2,25 @@ import React, { Component } from 'react';
 import Listado from './listado';
 
 class ListadoC extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lista:[]
+        }
+    }
+    componentDidMount(){
+        fetch("https://restcountries.eu/rest/v2/all") 
+        .then(data => {
+            return data.json(); 
+        }).catch(p=>0)
+        .then (data => {
+            this.setState({lista:data})
+        })
+    }
+
     render(){
         return(
-            <Listado/>
+            <Listado data={this.state.lista}/>
         );
     }
 }
