f427b1f8bb168d519b0287f0feef9eae3093cf6f
Julian Moreno <julian.moreno@globallogic.com>
2019-06-12 22:22:18 -0300

    prog-1 Se sube ultima actualizacion.

--- clase8/modulos/math/index.js
+++ clase8/modulos/math/index.js
@@ -7,21 +7,63 @@ var router = express.Router();
 //Forma correcta de llamar una funcion, es la manera asicronica, por eso hay 3 formas.
 
 //collback
-/*var res = math.suma(2,3,(err,data)=>{
-    if(err){
-        console.log("Error");
-    }
-    res = data;
-});*/
-
-//Promise
-router.get('/sumas/:num1-:num2', function (req, res, next) {
+router.get('/sumasCollback/:num1-:num2', function (req, res, next) {
     var num1 = Number(req.params.num1);
     var num2 = Number(req.params.num2);
 
     math.suma(num1, num2)
         .then(data => {
-            return math.resta(10, data);
+            return Promise.reject("Ejemplo reject promesa");//math.resta(10, data);
+        })
+        .then(data => {
+            math.division(2, data, (err, data) => {
+                if (err) {
+                    res.send("Error Division");
+                    return;
+                }
+                math.multiplicacion(20, data, (err, data) => {
+                    res.send("Resultado: " + data);
+                });
+            });
+        })
+        .catch(e => {
+            res.send(e);
+        })
+});
+
+//Promise
+router.get('/sumasPromise/:num1-:num2', function (req, res, next) {
+    var num1 = Number(req.params.num1);
+    var num2 = Number(req.params.num2);
+
+    math.suma(num1, num2)
+        .then(data => {
+            return Promise.reject("Ejemplo reject promesa");//math.resta(10, data);
+        })
+        .then(data => {
+            math.division(2, data, (err, data) => {
+                if (err) {
+                    res.send("Error Division");
+                    return;
+                }
+                math.multiplicacion(20, data, (err, data) => {
+                    res.send("Resultado: " + data);
+                });
+            });
+        })
+        .catch(e => {
+            res.send(e);
+        })
+});
+
+//Event
+router.get('/sumasEvent/:num1-:num2', function (req, res, next) {
+    var num1 = Number(req.params.num1);
+    var num2 = Number(req.params.num2);
+
+    math.suma(num1, num2)
+        .then(data => {
+            return Promise.reject("Ejemplo reject promesa");//math.resta(10, data);
         })
         .then(data => {
             math.division(2, data, (err, data) => {
